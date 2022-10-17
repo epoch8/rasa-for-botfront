fb7e78070c9cf193b26dae12762f26c1fa8644b4
baby-platom <platon200502@gmail.com>
2022-10-17 10:42:59 +0600

    vk-connect ready for tests

--- rasa/core/channels/vk.py
+++ rasa/core/channels/vk.py
@@ -1,15 +1,11 @@
 import aiohttp
 import logging
-from copy import deepcopy
 from sanic import Blueprint, response
 from sanic.request import Request
-from sanic.response import HTTPResponse
+from sanic.response import HTTPResponse, text
 from typing import Dict, Text, Any, List, Optional, Callable, Awaitable
 
 from rasa.core.channels.channel import InputChannel, UserMessage, OutputChannel
-from rasa.shared.constants import INTENT_MESSAGE_PREFIX
-from rasa.shared.core.constants import USER_INTENT_RESTART
-
 logger = logging.getLogger(__name__)
 
 
@@ -40,9 +36,8 @@ class VkOutput(OutputChannel):
             send_message_url = "https://api.vk.com/method/messages.send"
             async with session.post(send_message_url) as resp:
                 data = await resp.json()
-                if "error" not in data:
-                    return True
-                logger.error(f"Sending message ended up with error: {data['error']}")
+                if "error" in data:
+                    logger.error(f"Sending message ended up with error: {data['error']}")
                 return
                 
 
@@ -54,7 +49,8 @@ class VkOutput(OutputChannel):
 
     # TODO send_image_url
 
-    def _create_keyboard_from_buttons(buttons: List[Dict[Text, Any]]):
+    @staticmethod
+    def form_keyboard_buttons(buttons: List[Dict[Text, Any]]):
         def _chunkIt(seq, num):
             out = []
             last = 0
@@ -68,9 +64,9 @@ class VkOutput(OutputChannel):
             for i, piece in enumerate(row):
                 row[i] = {  
                     "action": {  
-                        "type": piece["title"],
+                        "type": "text",
                         "payload": piece["payload"],
-                        "label":"Red"
+                        "label": piece["title"]
                     }
                 }
 
@@ -90,85 +86,27 @@ class VkOutput(OutputChannel):
 
         :button_type reply: reply keyboard
         """
-        data = {
+        keyboard = {
             "one_time": False,
-            "buttons": []            
+            "buttons": self.form_keyboard_buttons(buttons)
         }
         if button_type == "inline":
-            reply_markup = InlineKeyboardMarkup()
-            button_list = [
-                InlineKeyboardButton(s["title"], callback_data=s["payload"])
-                for s in buttons
-            ]
-            reply_markup.row(*button_list)
-
-        elif button_type == "reply":
-            reply_markup = ReplyKeyboardMarkup(
-                resize_keyboard=False, one_time_keyboard=True
-            )
-            # drop button_type from button_list
-            button_list = [b for b in buttons if b.get("title")]
-            for idx, button in enumerate(buttons):
-                if isinstance(button, list):
-                    reply_markup.add(KeyboardButton(s["title"]) for s in button)
-                else:
-                    reply_markup.add(KeyboardButton(button["title"]))
-        else:
+            keyboard["inline"] = True
+        elif not button_type == "reply":
             logger.error(
                 "Trying to send text with buttons for unknown "
                 "button type {}".format(button_type)
             )
             return
-
-        self.send_message(user_id, message, reply_markup=reply_markup)
-
-    async def send_custom_json(
-        self, recipient_id: Text, json_message: Dict[Text, Any], **kwargs: Any
-    ) -> None:
-        json_message = deepcopy(json_message)
-
-        recipient_id = json_message.pop("chat_id", recipient_id)
-
-        send_functions = {
-            ("text",): "send_message",
-            ("photo",): "send_photo",
-            ("audio",): "send_audio",
-            ("document",): "send_document",
-            ("sticker",): "send_sticker",
-            ("video",): "send_video",
-            ("video_note",): "send_video_note",
-            ("animation",): "send_animation",
-            ("voice",): "send_voice",
-            ("media",): "send_media_group",
-            ("latitude", "longitude", "title", "address"): "send_venue",
-            ("latitude", "longitude"): "send_location",
-            ("phone_number", "first_name"): "send_contact",
-            ("game_short_name",): "send_game",
-            ("action",): "send_chat_action",
-            (
-                "title",
-                "decription",
-                "payload",
-                "provider_token",
-                "start_parameter",
-                "currency",
-                "prices",
-            ): "send_invoice",
-        }
-
-        for params in send_functions.keys():
-            if all(json_message.get(p) is not None for p in params):
-                args = [json_message.pop(p) for p in params]
-                api_call = getattr(self, send_functions[params])
-                api_call(recipient_id, *args, **json_message)
+        self.send_message(user_id, message, keyboard)
 
 
-class TelegramInput(InputChannel):
-    """Telegram input channel"""
+class VkInput(InputChannel):
+    """Vk input channel"""
 
     @classmethod
     def name(cls) -> Text:
-        return "telegram"
+        return "vk"
 
     @classmethod
     def from_credentials(cls, credentials: Optional[Dict[Text, Any]]) -> InputChannel:
@@ -178,122 +116,64 @@ class TelegramInput(InputChannel):
         return cls(
             credentials.get("access_token"),
             credentials.get("verify"),
-            credentials.get("webhook_url"),
+            credentials.get("secret_key")
         )
 
     def __init__(
         self,
         access_token: Optional[Text],
         verify: Optional[Text],
-        webhook_url: Optional[Text],
+        secret_key: Optional[Text],
         debug_mode: bool = True,
     ) -> None:
         self.access_token = access_token
         self.verify = verify
-        self.webhook_url = webhook_url
+        self.secret_key = secret_key
         self.debug_mode = debug_mode
-
-    @staticmethod
-    def _is_location(message) -> bool:
-        return message.location is not None
-
-    @staticmethod
-    def _is_user_message(message) -> bool:
-        return message.text is not None
-
-    @staticmethod
-    def _is_button(message) -> bool:
-        return message.callback_query is not None
 
     def blueprint(
         self, on_new_message: Callable[[UserMessage], Awaitable[Any]]
     ) -> Blueprint:
-        telegram_webhook = Blueprint("telegram_webhook", __name__)
+        vk_webhook = Blueprint("vk_webhook", __name__)
         out_channel = self.get_output_channel()
 
-        @telegram_webhook.route("/", methods=["GET"])
-        async def health(_: Request) -> HTTPResponse:
-            return response.json({"status": "ok"})
+        @vk_webhook.route("/", methods=["POST"])
+        async def webhook(request: Request) -> HTTPResponse:
+            data = request.json
+            if data["type"] == "confirmation":
+                return text(self.verify)
+            if "secret" not in data:
+                logger.error("A secret key is not set for the vk group")
+                return
+            if data["secret"] != self.secret_key:
+                logger.error("The secret key doesn't match the passed one")
+                return text("incorrect secret key")
 
-        @telegram_webhook.route("/set_webhook", methods=["GET", "POST"])
-        async def set_webhook(_: Request) -> HTTPResponse:
-            s = out_channel.setWebhook(self.webhook_url)
-            if s:
-                logger.info("Webhook Setup Successful")
-                return response.text("Webhook setup successful")
-            else:
-                logger.warning("Webhook Setup Failed")
-                return response.text("Invalid webhook")
-
-        @telegram_webhook.route("/webhook", methods=["GET", "POST"])
-        async def message(request: Request) -> Any:
-            if request.method == "POST":
-
-                request_dict = request.json
-                update = Update.de_json(request_dict)
-                if not out_channel.get_me().username == self.verify:
-                    logger.debug("Invalid access token, check it matches Telegram")
-                    return response.text("failed")
-
-                if self._is_button(update):
-                    msg = update.callback_query.message
-                    text = update.callback_query.data
-                else:
-                    msg = update.message
-                    if self._is_user_message(msg):
-                        text = msg.text.replace("/bot", "")
-                    elif self._is_location(msg):
-                        text = '{{"lng":{0}, "lat":{1}}}'.format(
-                            msg.location.longitude, msg.location.latitude
+            try:
+                if data["type"] == "message_new":
+                    message = data["object"]["message"]
+                    sender_id = data["from_id"]
+                    payload = message["text"]
+                    if "payload" in message:
+                        payload = dict(message["payload"])
+                    await on_new_message(
+                        UserMessage(
+                            payload,
+                            out_channel,
+                            sender_id,
+                            input_channel=self.name(),
                         )
-                    else:
-                        return response.text("success")
-                sender_id = msg.chat.id
-                metadata = self.get_metadata(request)
-                try:
-                    if text == (INTENT_MESSAGE_PREFIX + USER_INTENT_RESTART):
-                        await on_new_message(
-                            UserMessage(
-                                text,
-                                out_channel,
-                                sender_id,
-                                input_channel=self.name(),
-                                metadata=metadata,
-                            )
-                        )
-                        await on_new_message(
-                            UserMessage(
-                                "/start",
-                                out_channel,
-                                sender_id,
-                                input_channel=self.name(),
-                                metadata=metadata,
-                            )
-                        )
-                    else:
-                        await on_new_message(
-                            UserMessage(
-                                text,
-                                out_channel,
-                                sender_id,
-                                input_channel=self.name(),
-                                metadata=metadata,
-                            )
-                        )
-                except Exception as e:
-                    logger.error(f"Exception when trying to handle message.{e}")
-                    logger.debug(e, exc_info=True)
-                    if self.debug_mode:
-                        raise
-                    pass
+                    )
+            except Exception as e:
+                logger.error(f"Exception when trying to handle message.{e}")
+                logger.debug(e, exc_info=True)
+                if self.debug_mode:
+                    raise
 
-                return response.text("success")
+            return response.text("success")
 
-        return telegram_webhook
+        return vk_webhook
 
-    def get_output_channel(self) -> TelegramOutput:
-        """Loads the telegram channel."""
-        channel = TelegramOutput(self.access_token)
-        channel.set_webhook(url=self.webhook_url)
-
-        return channel
+    def get_output_channel(self) -> VkOutput:
+        """Loads the vk channel."""
+        return VkOutput(self.access_token)
